58ce50bb1812ed4acaa92ec43f2e0ffd300363be
barbara-tyndorf <barbaratyndorf@gmail.com>
2021-01-11 10:38:32 +0100

    update Guest entity

--- src/main/java/com/javagirls/MangoRoom/entity/Guest.java
+++ src/main/java/com/javagirls/MangoRoom/entity/Guest.java
@@ -56,41 +56,4 @@ public class Guest {
     @Column(name = "country")
     String country;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Guest guest = (Guest) o;
-        return Objects.equals(id, guest.id) &&
-                Objects.equals(firstName, guest.firstName) &&
-                Objects.equals(lastName, guest.lastName) &&
-                Objects.equals(idCardNumber, guest.idCardNumber) &&
-                Objects.equals(phoneNumber, guest.phoneNumber) &&
-                Objects.equals(street, guest.street) &&
-                Objects.equals(number, guest.number) &&
-                Objects.equals(postalCode, guest.postalCode) &&
-                Objects.equals(city, guest.city) &&
-                Objects.equals(country, guest.country);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, firstName, lastName, idCardNumber, phoneNumber, street, number, postalCode, city, country);
-    }
-
-    @Override
-    public String toString() {
-        return "Guest{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", idCardNumber='" + idCardNumber + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", street='" + street + '\'' +
-                ", number=" + number +
-                ", postalCode='" + postalCode + '\'' +
-                ", city='" + city + '\'' +
-                ", country='" + country + '\'' +
-                '}';
-    }
 }

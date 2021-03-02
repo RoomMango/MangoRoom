437dd89a49f251eee9977b1306ed1e0ab707d5cb
barbara-tyndorf <barbaratyndorf@gmail.com>
2021-03-02 11:29:21 +0100

    refactor findAllRooms method to returns dto

--- src/test/java/com/javagirls/MangoRoom/service/RoomServiceTest.java
+++ src/test/java/com/javagirls/MangoRoom/service/RoomServiceTest.java
@@ -2,15 +2,22 @@ package com.javagirls.MangoRoom.service;
 
 import com.javagirls.MangoRoom.dto.RoomDto;
 import com.javagirls.MangoRoom.entity.Room;
+import com.javagirls.MangoRoom.mock.RoomMock;
 import com.javagirls.MangoRoom.repository.RoomRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.util.Assert;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -20,7 +27,6 @@ class RoomServiceTest {
     private RoomRepository repository;
     @Autowired
     private RoomService service;
-
 
     @Test
     void changeRoomStatus_fromTrueToFalse() {
@@ -42,5 +48,15 @@ class RoomServiceTest {
         when(repository.findById(room.getRoomNumber())).thenReturn(java.util.Optional.of(room));
         RoomDto roomDTO = service.changeRoomStatus(room.getRoomNumber());
         assertThat(roomDTO.isAvailableForBooking()).isEqualTo(true);
+    }
+
+    @Test
+    void findAllRooms_should_return_roomDto_list() {
+        Room room1 = RoomMock.getBasicRoom();
+        Room room2 = RoomMock.getBasicRoom();
+        repository.saveAll(Arrays.asList(room1, room2));
+        List<RoomDto> list = new ArrayList<>();
+        assertEquals(list.getClass(), service.findAllRooms().getClass());
+
     }
 }
